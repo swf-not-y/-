1e844a33e4e6645b0783c710b2fa498674fc384d
swf-not-y <1873366265@qq.com>
2021-08-07 21:03:24 +0800

    Create ZiFuChuanLianJie.java

---
+++ javase第一章/ZiFuChuanLianJie.java
@@ -0,0 +1,11 @@
+//当+在字符串中时是连接作用
+public class ZiFuChuanLianJie {
+	public static void main(String[] args){
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		String name = "zhangshan";
+		System.out.println(a +"+"+ b + "="+(a+b));
+		System.out.println("欢迎回家，"+name);
+	}
+}
