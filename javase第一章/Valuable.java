d4dc834877c13edf8326e11d8ed31db9e0ac9de3
swf-not-y <1873366265@qq.com>
2021-08-07 21:03:11 +0800

    Create Valuable.java

---
+++ javase第一章/Valuable.java
@@ -0,0 +1,30 @@
+
+public class Valuable {
+	static int i = 1;
+	static int b = 10;
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 200;
+		int c = 0;
+		c = a + b;
+		System.out.println(c);
+		int i = 100;
+		System.out.println(i);
+		i = 200;
+		System.out.println(i);
+		jisuan();
+		/*
+		System.out.println(i);
+		*/
+	}
+	public static void jisuan(){
+		System.out.println(i);
+		System.out.println(b);
+		
+	}
+}
+class 学生{
+	public static void main (String[] args){
+		System.out.println("你说的哈");
+	}
+}
