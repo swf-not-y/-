22f6a097461d859d0ef449ab55aa997c290f130f
swf-not-y <1873366265@qq.com>
2021-08-07 21:03:20 +0800

    Create ZhuanYiZiFu.java

---
+++ javase第一章/ZhuanYiZiFu.java
@@ -0,0 +1,20 @@
+
+public class ZhuanYiZiFu {
+	public static void main(String[] args){
+		char c1 = 'n';
+		System.out.println(c1);
+		char c2 = '\n';
+		char c3 = '\t';
+		char c4 = '\\';
+		//System.out.println(c2);
+		System.out.print("Hello");
+		System.out.println(c3);
+		System.out.println("Hello");
+		System.out.println(c4);
+		System.out.println("¡°Hello¡±");
+		
+
+		//System.out.println(""Hello"");
+
+	}
+}
