504dcaf2a1479f04cb359573b830f28999bd323d
swf-not-y <1873366265@qq.com>
2021-08-07 21:02:33 +0800

    Create Boolean.java

---
+++ javase第一章/Boolean.java
@@ -0,0 +1,12 @@
+
+public class Boolean {
+	public static void main(String[] args){
+		boolean flag = true;
+		if(flag){
+			System.out.println("ÄãºÃ£¬»¶Ó­Äã");
+		}
+		else{
+			System.out.println("¹ö");
+		}
+	}
+}
