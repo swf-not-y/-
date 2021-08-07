67fd9cb8196ebc1a01d64ccad0e31096980be8f7
swf-not-y <1873366265@qq.com>
2021-08-07 21:03:06 +0800

    Create SanYuan.java

---
+++ javase第一章/SanYuan.java
@@ -0,0 +1,7 @@
+//三目运算符---布尔表达式：。。。？表达式1：表达式2，如果。。。为真，则输出表达式一。反之表达式二
+public class SanYuan {
+	public static void main(String[] args){
+		boolean sex = true;
+		System.out.println(sex ? '男':'女');
+	}
+}
