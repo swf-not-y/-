e77f3e7910d075bfbe45b38b5eb36809bf40dd65
swf-not-y <1873366265@qq.com>
2021-08-07 21:02:54 +0800

    Create LuoLi.java

---
+++ javase第一章/LuoLi.java
@@ -0,0 +1,16 @@
+
+public class LuoLi {
+	public static void main(String[] args){
+		System.out.println(6>3&5>2);
+		System.out.println(6>3|5>2);
+		System.out.println(6>3&&5>2);//¶ÌÂ·Óë
+		
+		int x = 10;
+		int y = 9;
+		System.out.println(x>y&++x<y);
+		System.out.println(x);
+		System.out.println(x>y&&++x<y);
+		System.out.println(x);
+		
+	}
+}
