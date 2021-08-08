2aa83aecde9a3236938febf3a83551d9898c741b
swf-not-y <1873366265@qq.com>
2021-08-08 21:57:32 +0800

    Create DiGui.java

---
+++ 方法/src/DiGui.java
@@ -0,0 +1,29 @@
+//求1到n之间的数字的和
+public class DiGui {
+	public static void main(String[] args){
+		java.util.Scanner s = new java.util.Scanner(System.in);
+		System.out.println("请输入一个数：");
+		int n = s.nextInt();
+		int b = sum(n);
+		int c = JieChen(n);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+	public static int sum(int n){
+		if (n==1){
+			return n;
+		}
+		else{
+			return n + sum(n-1);
+		}
+	}
+	public static int JieChen(int n){
+		if (n == 1){
+			return n;
+		}
+		else{
+			return n*JieChen(n-1);
+		}
+	}
+}
