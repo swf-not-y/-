a393846967e0bbec6c31e31b04d87e7a10b3a14b
swf-not-y <1873366265@qq.com>
2021-08-08 21:57:35 +0800

    Update Method2.java

--- 方法/src/Method2.java
+++ 方法/src/Method2.java
@@ -5,6 +5,11 @@ public class Method2 {
 		Method3.cenfa(29,43);
 	}
 	public static void sum(int x,int y){
+		java.util.Scanner s = new java.util.Scanner(System.in);
+		System.out.println("请输入x的值");
+		x = s.nextInt();
+		System.out.println("请输入y的值");
+		y = s.nextInt();
 		int c = x + y;
 		System.out.println(c);
 	}
