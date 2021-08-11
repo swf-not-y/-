7ffa6a29cc1123d6942b57562ea0e4cadef1e49e
swf-not-y <1873366265@qq.com>
2021-08-11 22:01:08 +0800

    Create Text4.java

---
+++ javase/src/Text4.java
@@ -0,0 +1,10 @@
+
+public class Text4 {
+	public static void main(String[] args){
+		User a = new User();
+		User b = new User(10);
+		User c = new User("уехЩ");
+		//System.out.println(a.User());
+
+	}
+}
