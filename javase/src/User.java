9e334e2ab5ede76f7bd783e7533d5905d86cc3a9
swf-not-y <1873366265@qq.com>
2021-08-11 22:01:22 +0800

    Create User.java

---
+++ javase/src/User.java
@@ -0,0 +1,19 @@
+
+public class User {
+	public User(){
+		System.out.println("构造方法");
+	}
+	
+	public User(int i){
+		System.out.println("带参数的构造方法");
+	}
+	
+	public User(String name){
+		System.out.println("你好");
+	}
+
+	/*public char[] User() {
+		// TODO Auto-generated method stub
+		return null;
+	}*/
+}
