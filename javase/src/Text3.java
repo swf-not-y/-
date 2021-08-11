4fa06fa5ce338e46d485b60bb6d528cad4070c9d
swf-not-y <1873366265@qq.com>
2021-08-11 22:01:04 +0800

    Update Text3.java

--- javase/src/Text3.java
+++ javase/src/Text3.java
@@ -1,7 +1,7 @@
 
 public class Text3 {
 	public static void main(String[] args){
-		Age age = new Age();
+		Age age = new Age();//运用构造方法构造一个对象，其中Age是类型
 		age.setaAge(100);
 		System.out.println(age.getAge());
 
