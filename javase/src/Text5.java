3ae9c30f68d81a769cdd055d79d935d2d942757b
swf-not-y <1873366265@qq.com>
2021-08-11 22:01:11 +0800

    Create Text5.java

---
+++ javase/src/Text5.java
@@ -0,0 +1,13 @@
+
+public class Text5 {
+	public static void main(String[] args){
+		int i = 10;
+		add(i);
+		System.out.println("add -->"+i);
+	}
+	
+	public static void add(int i){
+		i++;
+		System.out.println("add -->"+i);
+	}
+}
