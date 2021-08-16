1051583baacbb09f942d1ce3092ff7d665cb5945
swf-not-y <1873366265@qq.com>
2021-08-16 22:42:39 +0800

    Create OverrideText.java

---
+++ 覆盖/OverrideText.java
@@ -0,0 +1,17 @@
+package ¸²¸Ç;
+
+public class OverrideText {
+	public static void main(String[] args){
+		Animal a = new Animal();
+		a.move();
+		
+		Cat c = new Cat();
+		c.move();
+		
+		Bird b = new Bird();
+		b.move();
+		
+		YingWu y  = new YingWu();
+		y.move();
+	}
+}
