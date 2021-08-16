b63b1572fd77b7fd714875e71dfc6551195d067f
swf-not-y <1873366265@qq.com>
2021-08-16 22:43:46 +0800

    Create Account.java

---
+++ javase/Account.java
@@ -0,0 +1,27 @@
+
+public class Account {
+	private String actno;
+	private double balance;
+	public Account(String actno, double balance) {
+		super();
+		this.actno = actno;
+		this.balance = balance;
+	}
+	
+	public Account() {
+		super();
+	}
+
+	public String getActno() {
+		return actno;
+	}
+	public void setActno(String actno) {
+		this.actno = actno;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+}
