cd390eb6872bb2f3968b2baceac1bbe227344fbc
swf-not-y <1873366265@qq.com>
2021-08-16 22:43:35 +0800

    Create Chinese.java

---
+++ javase第二章/Chinese.java
@@ -0,0 +1,13 @@
+
+public class Chinese {
+	String id;
+    String name;
+	static String country = "ÖÐ¹ú";
+	
+	public Chinese(String id, String name){
+		this.id = id;
+		this.name = name;
+		this.country = country;
+	}
+	
+}
