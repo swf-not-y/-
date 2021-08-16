64619a60e0cf22c798237a8086f4fa561312ec04
swf-not-y <1873366265@qq.com>
2021-08-16 22:42:51 +0800

    Create Text1.java

---
+++ 多态/Text1.java
@@ -0,0 +1,11 @@
+package ∂‡Ã¨;
+
+public class Text1 {
+	public static void main(String[] args){
+		People zhuren = new People();
+		Cat1 c = new Cat1();
+		zhuren.feed(c);
+		Dog erha = new Dog();
+		zhuren.feed(erha);
+	}
+}
