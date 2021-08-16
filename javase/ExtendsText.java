0407c8eb80e7792d06f8d2d1daa7b8b2b8d07aca
swf-not-y <1873366265@qq.com>
2021-08-16 22:43:39 +0800

    Create ExtendsText.java

---
+++ javase/ExtendsText.java
@@ -0,0 +1,12 @@
+
+public class ExtendsText {
+	public static void main(String[] args){
+		ExtendsText et = new ExtendsText();
+		String s = et.toString();
+		System.out.println(s);
+		CreditAccount a = new CreditAccount();
+		System.out.println(a.getCredit());
+		System.out.println(a.getActno());
+
+	}
+}
