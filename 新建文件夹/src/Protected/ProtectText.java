7dec1bcc9f848738c9b6c7e53e55dc12b49a962e
swf-not-y <1873366265@qq.com>
2021-08-19 21:19:19 +0800

    Create ProtectText.java

---
+++ 新建文件夹/src/Protected/ProtectText.java
@@ -0,0 +1,9 @@
+package Protected;
+
+public class ProtectText {
+	public static void main(String[] args){
+	Protect1 p = new Protect1();
+	System.out.println(p.i);
+	System.out.println(p.j);
+	}
+}
